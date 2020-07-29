124d5f351589a906fd70a4ccf42521e7af1690c5
Poliana Rufatto <poliana.rufatto@softplan.com.br>
2020-07-29 05:40:44 -0300

    Manupulando dados/Trabalhando com datas

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const routes = require("./routes");
 
 const server = express();
 
+server.use(express.urlencoded({ extended: true}));
 server.use(express.static('public'));
 server.use(routes);
 
@@ -14,53 +15,6 @@ nunjucks.configure("views", {
     autoescape: false,
     noCache: true
 });
-
-// server.get("/", function(req, res) {
-
-//     const about = {
-//         avatar_url: "https://avatars1.githubusercontent.com/u/5182966?s=460&u=ae8da8137a4e8f19a68f9df693cf409b304d5b7a&v=4",
-//         name: "Poliana Rufatto",
-//         role: "Automatizadora de testes - Softplan",
-//         description: 'Estudante de programação, focada em front-end. Colaboradora da <a href="https://www.softplan.com.br/" target="_blank">Softplan</a>',
-//         links: [
-//             {
-//                 name: "Github",
-//                 url: "https://github.com/PolianaRufatto"
-//             },
-//             {
-//                 name: "Linkedin",
-//                 url: "https://www.linkedin.com/in/polianarufatto/"
-//             },
-//             {
-//                 name: "Twitter",
-//                 url: ""
-//             },
-
-//         ]
-//     }
-
-//     return res.render('about', {about});
-// });
-
-// server.get("/portfolio", function(req, res) {
-//     return res.render('portfolio', { items: videos});
-// });
-
-// server.get("/video", function(req, res) {
-//     const id = req.query.id;
-
-//     const video = videos.find(function(video){
-//         return video.id === id;
-//     });
-
-//     if (!video) {
-//         return res.send("Video not found!");
-//     }
-
-//     return res.render("video", { item: video });
-
-//     res.send(id);
-// })
 
 server.listen(5000, function() {
     console.log('server is running');
